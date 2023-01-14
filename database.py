8dfb3e8b6713af3888f3332f1854be4df5b0597c
108-Software <75811746+108-Software@users.noreply.github.com>
2023-01-14 09:38:51 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,4 +1,5 @@
 import sqlite3 as sql
+from datetime import datetime
 
 class database(object):
 
@@ -6,7 +7,7 @@ class database(object):
         super(database, self).__init__()
 
     @staticmethod
-    def creating_database(day, month, year, time, status, temperature, status_weat, speed_wind):
+    def creating_database(day, month, year, status, temperature, status_weat, speed_wind):
         Month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
         dbase = sql.connect(str(day) + " " + Month[month-1] + " " + str(year) + ".db")
         with dbase:
@@ -19,7 +20,8 @@ class database(object):
                 'Состояние погоды' TEXT,
                 'Скорость ветра' TEXT);
                 """)
-
+            time = str(datetime.today().hour) + ":" + str(datetime.today().minute)
+            print(time)
             results = (time, status, temperature, status_weat, speed_wind)
 
             pointer.execute("INSERT INTO Weather VALUES(?, ?, ?, ?, ?);", results)
